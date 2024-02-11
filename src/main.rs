b1c51e485a5014a3f8d9a0110ba1a13601172756
Thedarkness-bd <toisiftasrik@gmail.com>
2024-02-12 04:27:52 +0600

    Learned the difference between print! and println! also learned comment

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
 fn main() {
     // this is the main function
     println!("Tousif Tasrik");
+    print!("Tousif loves to do coding\n");
+    println!("I also do");
+    print!("Heyyyy")
+    /*
+        This will be our multiline comment :))
+    
+     */
 }
